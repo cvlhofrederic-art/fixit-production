69c2d03149818526681cdd88bbf437c58258a374
Frédéric Carvalho <elgato_fofo@MBP-de-Frederic.lan>
2026-04-27 14:50:15 +0100

    feat(simulateur-v2): add data-integrity test suite (9 invariants)

--- lib/prix-travaux-2026.ts
+++ lib/prix-travaux-2026.ts
@@ -4,3 +4,12 @@ export * from './prix-travaux-2026/types'
 export * from './prix-travaux-2026/coefficients'
 export * from './prix-travaux-2026/region-detector'
 export * from './prix-travaux-2026/aides'
+
+import type { PriceLine } from './prix-travaux-2026/types'
+
+// Will be populated as métiers are added in Tasks 8-17.
+// Once all data files exist, this becomes:
+//   import { peintureLines } from './prix-travaux-2026/data/peinture'
+//   ...
+//   export const PRIX_2026: PriceLine[] = [...peintureLines, ...plomberieLines, ...]
+export const PRIX_2026: PriceLine[] = []
